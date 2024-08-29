8463b2de5e1c36f989fead660546ae6ac10d7de3
TheRayanFikri <129669770+TheRayanFikri@users.noreply.github.com>
2024-08-29 16:54:05 +0100

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -1,1 +1,15 @@
+from tkinter import *
 
+#Creating a window:
+win = Tk()
+
+#Window Customization:
+win.geometry('900x600')
+win.title('Gnome Auto Theming')
+win.state('normal')
+win.config(background='grey')
+
+#
+
+#Running the window (win):
+win.mainloop()
