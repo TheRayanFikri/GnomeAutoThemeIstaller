b387aefb90d141ea02c70644c89cd9a9695e00d2
TheRayanFikri <TheRayanFikri@gmail.com>
2024-09-01 17:39:29 +0100

    Rename Launcher.py to launcher.py

---
+++ README.txt
@@ -0,0 +1,6 @@
+README:
+Hello , my name is Rayane yeah I made this Tweak to make it easy for you to download themes.
+Before you start using this Tweaks , you should know that maybe Every Single Thing doesn't work.
+this is because of the tweak being still in development.
+When i will finish at least 3 or 4 themes buttons that are working I will be realising it officialy...
+Bye👋
--- launcher.py
+++ launcher.py
@@ -5,11 +5,76 @@ win = Tk()
 
 #Window Customization:
 win.geometry('900x600')
+win.minsize(900 , 600)
+win.maxsize(900, 600)
 win.title('Gnome Auto Theming')
 win.state('normal')
 win.config(background='grey')
 
-#
+#welcoming Label:
+welcomeLabel = Label(win , text='Welcome to Gnome Auto-Theming tweaker !' , bg='grey' , font=('Courrier' , 20) , foreground='White')
+welcomeLabel.pack()
+
+#TopFrame:
+topFrame = Frame(win , background='#c0c5c0')
+topFrame.pack(fill=BOTH, side=TOP )
+
+#Themes:
+
+#whiteSur:
+whiteSurFrame = Frame(topFrame , background='#c9c5b0')
+whiteSurFrame.pack(padx=5 , side=LEFT)
+
+whiteSurLabel = Label(whiteSurFrame , text='WhiteSur' , bg='#c9c5b0' , font=('Courrier' , 17))
+whiteSurLabel.pack()
+
+whiteSurIconsButton = Button(whiteSurFrame , text='Icons' , bg='White' , foreground='Black' , font=("Courrier" , 15))
+whiteSurThemesButton = Button(whiteSurFrame , text='Themes' , bg='White' , foreground='Black' , font=("Courrier" , 15))
+
+whiteSurThemesButton.pack()
+whiteSurIconsButton.pack()
+
+#Orchis:
+OrchisFrame = Frame(topFrame , background='#c9c5b0')
+OrchisFrame.pack(padx=5 , side=LEFT)
+
+OrchisLabel = Label(OrchisFrame, text='Orchis' , bg='#c9c5b0' , font=('Courrier' , 17))
+OrchisLabel.pack()
+
+OrchisIconsButton = Button(OrchisFrame , text='Icons' , bg='White' , foreground='Black' , font=("Courrier" , 15))
+OrchisThemesButton = Button(OrchisFrame , text='Themes' , bg='White' , foreground='Black' , font=("Courrier" , 15))
+
+OrchisThemesButton.pack()
+OrchisIconsButton.pack()
+
+#Vimix
+vimixFrame = Frame(topFrame , background='#c9c5b0')
+vimixFrame.pack(padx=5 , side=LEFT)
+
+vimixLabel = Label(vimixFrame, text='Vimix' , bg='#c9c5b0' , font=('Courrier' , 17))
+vimixLabel.pack()
+
+vimixIconsButton = Button(vimixFrame , text='Icons' , bg='White' , foreground='Black' , font=("Courrier" , 15))
+vimixThemesButton = Button(vimixFrame , text='Themes' , bg='White' , foreground='Black' , font=("Courrier" , 15))
+
+vimixThemesButton.pack()
+vimixIconsButton.pack()
+
+#Yaru :
+yaruFrame = Frame(topFrame , background='#c0c5b0')
+yaruFrame.pack(padx=5 , side=LEFT)
+
+yaruLabel = Label(yaruFrame , text='Yaru' , bg='#c0c5b0' ,font=('Courrier' , 17) , fg="Black")
+yaruLabel.pack()
+
+yaruIconsButton = Button(yaruFrame , text='Icons' ,bg='White' , foreground='Black' , font=('Courrier' , 15))
+yaruThemesButton = Button(yaruFrame , text='Themes' , bg='white' , foreground='Black' , font=('Courrier' , 15))
+
+yaruThemesButton.pack()
+yaruIconsButton.pack()
+
+#Other Theme:
+themenameFrame = Frame(topFrame , background='#c0c5b0')
 
 #Running the window (win):
 win.mainloop()
